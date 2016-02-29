75e809ceb891977d3b2a842edeebeaae1a96b076
Pierce Lamb <richard.pierce.lamb@gmail.com>
2016-02-28 16:45:36 -0800

    working through updatestatebykey

--- src/main/scala/main/main.scala
+++ src/main/scala/main/main.scala
@@ -1,8 +1,10 @@
 package main
 
+import java.text.SimpleDateFormat
+
 import com.datastax.spark.connector.cql.CassandraConnector
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{StateSpec, Seconds, StreamingContext}
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.log4j.{Logger, Level}
 
@@ -10,19 +12,31 @@ import org.apache.log4j.{Logger, Level}
  * Created by plamb on 2/27/16.
  */
 object main {
-//Case class for handling log events
-  case class LogEvent(ip:String, timestamp:String, requestPage:String, responseCode:Int, responseSize:Int, userAgent:String)
+  val dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z")
+
+//Case classes for handling log events
+  case class LogEvent(ip:String, timestamp:Long, requestPage:String, responseCode:Int, responseSize:Int, userAgent:String)
+
 //Function to parse text logs into the case class
   def parseLogEvent(event: String): LogEvent = {
     val LogPattern = """^([\d.]+) (\S+) (\S+) \[(.*)\] \"([^\s]+) (/[^\s]*) HTTP/[^\s]+\" (\d{3}) (\d+) \"([^\"]+)\" \"([^\"]+)\"$""".r
     val m = LogPattern.findAllIn(event)
     if (m.nonEmpty)
-      new LogEvent(m.group(1), m.group(4), m.group(6), m.group(7).toInt, m.group(8).toInt, m.group(10))
+      new LogEvent(m.group(1), dateFormat.parse(m.group(4)).getTime(), m.group(6), m.group(7).toInt, m.group(8).toInt, m.group(10))
     else
       null
   }
 
-  //val dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z")
+  //Function passed to UpdateStateByKey. Using an IP as a key, accumulates a list of pages hit and times on page.
+  def updateValues(
+                    newValues: Seq[(String, Long, Long)],
+                    currentValue: Option[Seq[(String, Long, Long)]]
+                    ):
+                    Option[Seq[(String, Long, Long)]] = {
+
+    Some(currentValue.getOrElse(Seq.empty) ++ newValues)
+  }
+
 
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("akka").setLevel(Level.OFF)
@@ -46,10 +60,54 @@ def main(args: Array[String]) {
   val events = KafkaUtils.createStream(ssc,"localhost:2181","sparkFetcher",Map("apache"->2))
     .map(_._2) // grab just the web log from the stream
 
+  //Parse the raw logs and filter for the pages we care about. Return k/v pairs of sessions
   val parseLogs = events.flatMap(_.split("\n")).map(parseLogEvent)
+    .filter(
+      logevent => logevent.requestPage.endsWith(".html") ||
+                  logevent.requestPage.endsWith(".php") ||
+                  logevent.requestPage.equals("/")
+    ).map{
+    event => (event.ip, (event.requestPage, event.timestamp, event.timestamp))
+  }
 
-  parseLogs.print()
+  ssc.checkpoint("/home/plamb/Coding/SnappyData/My_Projects/misc/sparkweblognew-checkpoint")
 
+  //Combine the keys and group pages times into a Map via the pages themselves.
+  val ipToPage = parseLogs.updateStateByKey(updateValues).map{event => (event._1, event._2.groupBy(_._1))}
+
+  val combineTimes = ipToPage.map{session =>
+
+    val lol = session._2.mapValues{
+          case Nil => None
+          case (_, time1, time2) :: tail => //combineTimes(tail, time1, time2)
+            Some(tail.foldLeft((time1, time2)) {
+              case ((startTime, nextStartTime), (_, endTime, nextEndTime)) =>
+                (startTime min nextStartTime, endTime max nextEndTime)
+            })
+    }
+
+    (session._1, lol)
+
+  }
+
+
+//    .mapValues{
+//    case Nil => None
+//    case (_, time1, time2) :: tail => //combineTimes(tail, time1, time2)
+//      Some(tail.foldLeft((time1, time2)) {
+//        case ((startTime, nextStartTime), (_, endTime, nextEndTime)) =>
+//          (startTime min nextStartTime, endTime max nextEndTime)
+//      })
+//  }}
+
+
+  ipToPage.print()
+
+//  def combineTimes(timesListTail: List[(String, Long, Long)], time1: Long, time2: Long) = {
+//    timesListTail.foldLeft((time1, time2)) =>
+//      case ((startTime, nextStartTime), (_, endTime, nextEndTime)) =>
+//        (startTime.  nextStartTime, endTime max nextEndTime)
+//  }
 
   ssc.start()
   ssc.awaitTermination()
